db1704d7b6db1472fdf5256ce0b24a89231f7b0b
Pradeep Kumar Singha <mr.pradeepkumarsingha@gmail.com>
2026-03-21 11:43:38 +0530

    fixing error

--- models/app.py
+++ models/app.py
@@ -90,5 +90,10 @@ def predict():
         return jsonify({"error": str(e)}), 500
 
 
+# if __name__ == "__main__":
+#     app.run(debug=True)
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Render provides PORT
+    app.run(host="0.0.0.0", port=port,debug=False)
